9eb9e04ac3fb6362566d324f6fa54bdbac3fb0f7
Jay Pak <sleepingpaili@gmail.com>
2018-09-22 08:49:52 +0000

    Upload New File

---
+++ OCT Program With GNUPlot 2/OCTProgramAbou.cpp
@@ -0,0 +1,34 @@
+// OCTProgramAbou.cpp : 구현 파일입니다.
+//
+
+#include "stdafx.h"
+#include "OCT Program With GNUPlot 2.h"
+#include "OCTProgramAbou.h"
+#include "afxdialogex.h"
+
+
+// OCTProgramAbou 대화 상자입니다.
+
+IMPLEMENT_DYNAMIC(OCTProgramAbou, CDialogEx)
+
+OCTProgramAbou::OCTProgramAbou(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_ABOUTBOX, pParent)
+{
+
+}
+
+OCTProgramAbou::~OCTProgramAbou()
+{
+}
+
+void OCTProgramAbou::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(OCTProgramAbou, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// OCTProgramAbou 메시지 처리기입니다.
