43c4791ea711188f6add6ca4b89850ef13b8da76
Jay Pak <sleepingpaili@gmail.com>
2018-09-22 08:50:08 +0000

    Upload New File

---
+++ OCT Program With GNUPlot 2/resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// OCT Program With GNUPlot 2.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_OCTPROGRAMWITHGNUPLOT2_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDD_DIALOG1                     132
+#define IDD_ORTHOGONAL                  134
+#define IDC_EDIT_X                      1000
+#define IDC_EDIT_Y                      1001
+#define IDC_EDIT_Z                      1002
+#define IDC_VOLUME                      1003
+#define IDC_DISTANCE                    1004
+#define IDC_EDIT1                       1005
+#define IDC_BTN_                        1006
+#define IDC_BTN_ORTHOGONAL              1006
+#define IDC_BTN_X_AXIS                  1007
+#define IDC_BTN_Y_AXIS                  1008
+#define IDC_BTN_Z_AXIS                  1009
+#define ID_FILE_OPEN32771               32771
+#define ID_FILE_EXIT                    32772
+#define ID_OPEN_VIDEOFILE               32773
+#define ID_OPEN_IMAGEFILE               32774
+#define ID_IMAGEFILE_CIRRUSOCT          32775
+#define ID_IMAGEFILE_SPECTRALIS         32776
+#define ID_HELP_INTRODUCTION            32777
+#define ID_HELP_ABOUT                   32778
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
